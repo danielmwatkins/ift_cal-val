5cb3e9d68a3e297c481ebda0982433031f13a3d3
daniel watkins <danielmwatkins@gmail.com>
2024-01-25 11:23:49 -0500

    Labeled geotiffs

--- README.md
+++ README.md
@@ -14,7 +14,8 @@ The calibration and validation code includes both Python and Julia scripts. The 
 
 The package `proplot` is used for figures. There are issues with `proplot` for newer versions of `matplotlib`, hence the version is set as 0.9.7 for `proplot` and 3.4.3 for `matplotlib`. If a newer version of `scikit-image` is needed, we may need to choose a different tool for figure creation.
 
-For Julia, `IceFloeTracker.jl` requires at least Julia 1.9. Instructions for installing IFT are on the IFT github repository. TBD: Check whether we need to explicitly include `iJulia` in the `micromamba` yaml file in order to run Julia notebooks.
+For Julia, `IceFloeTracker.jl` requires at least Julia 1.9. Instructions for installing IFT are on the IFT github repository.
+TBD: Check whether we need to explicitly include `iJulia` in the `micromamba` yaml file in order to run Julia notebooks.
 
 ## Sample selection
 Sample selection is described and carried out in the Jupyter notebook `sample_selection.ipynb`. The notebook produces the following:
---
+++ scripts/convert_labeled_png_to_geotiff.py
@@ -0,0 +1,55 @@
+# The labeled floe images are produced in Photoshop and
+# are designed to have the exact dimentions
+# Naming issue: would be easier if the region names didn't have underscores
+
+import rasterio
+import os
+import pandas as pd
+
+# Example:
+png_loc = '../data/validation_images/labeled_floes_png/'
+save_loc = '../data/validation_images/labeled_floes_gtiff/'
+ref_loc = '../data/validation_images/truecolor/'
+
+labeled_images = [f for f in os.listdir(png_loc) if 'png' in f]
+
+site_defs = pd.read_csv('../data/location_specifications/region_definitions.csv')
+regions = list(site_defs['location'].values)
+
+for lb_image in labeled_images:
+    for region in regions:
+        if region in lb_image:
+            # stick a hyphen in where the _ is
+            # TBD: update naming to not have _ so that this isn't needed
+            cn, reg, date, sat = lb_image.replace(region, region.replace('_', '')).split('_')
+            sat = sat.replace('.png', '')
+            break
+    ref_image = '_'.join([cn, region, '100km', date]) + '.' + sat + '.truecolor.250m.tiff'
+    
+
+    with rasterio.open(ref_loc + ref_image) as src_dataset:
+        
+        # Get a copy of the source dataset's profile. Thus our
+        # destination dataset will have the same dimensions,
+        # number of bands, data type, and georeferencing as the
+        # source dataset.
+        kwds = src_dataset.profile
+    
+        # Change the format driver for the destination dataset to
+        # 'GTiff', short for GeoTIFF.
+        kwds['driver'] = 'GTiff'
+    
+        # Add GeoTIFF-specific keyword arguments.
+        kwds['tiled'] = True
+        kwds['blockxsize'] = 256
+        kwds['blockysize'] = 256
+        kwds['photometric'] = 'MINISBLACK'
+        kwds['compress'] = 'PNG'
+    
+        # Floe data only has 1 layer (binary data)
+        kwds['count'] = 1
+
+    labeled = rasterio.open(png_loc + lb_image)
+    data = labeled.read(1)
+    with rasterio.open(save_loc + lb_image.replace('png', 'tiff'), mode='w', **kwds) as new_file:
+        new_file.write(data, 1)
